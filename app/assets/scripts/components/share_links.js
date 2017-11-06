3aac57c9583185ad6bca85d0542f250edddc8912
Jarod <jarod.hargreaves@gmail.com>
2017-11-06 21:46:46 +0000

    Put ShareLink into function
    Component unneccessary for now

--- app/assets/scripts/components/share_links.js
+++ app/assets/scripts/components/share_links.js
@@ -1,26 +1,28 @@
-import React, { Component } from "react"
+import React from "react"
 
-class ShareLinks extends Component {
-    render() {
-        return (
-            <div className="poem-links">
-                <a
-                    className="poem-links--share"
-                    href={shareLink(this.props.title, this.props.lines, this.props.chosenLines)}
-                >
-                    <i className="material-icons md-48">share</i>
-                </a>
-            </div>
-        )
-    }
-}
+const ShareLinks = ({ title, lines, chosenLines }) => (
+    <div className="poem-links">
+        <a
+            className="poem-links--share"
+            href={shareLink(title, lines, chosenLines)}
+        >
+            <i className="material-icons md-48">share</i>
+        </a>
+    </div>
+)
+
 export default ShareLinks
+
+//////////////////////////////////
+//      PRIVATE FUNCTIONS       //
+//////////////////////////////////
 
 function shareLink(title, lines, chosenLines) {
     const whatsAppPre = "whatsapp://send?text="
     const whatsAppPost =
         "\nMake your own autopoem at jarodhargreav.es/autopoetry"
-    let poemText = `${title}\n\n`
+
+    let poemText = `${title.toUpperCase()}\n\n`
 
     chosenLines.forEach(line => {
         poemText += `${lines[line]}\n`
@@ -30,34 +32,3 @@ function shareLink(title, lines, chosenLines) {
 
     return whatsAppLink
 }
-
-// class ShareLinks {
-//     constructor() {}
-//     newPoem() {
-//         this.poemContents = $(".poem-line--text")
-//         this.poemTitle = $(".poem-title h1")
-//             .text()
-//             .toUpperCase()
-//     }
-
-//     whatsAppPoemLink() {
-//         const whatsAppPre = "whatsapp://send?text="
-//         const whatsAppPost =
-//             "\nMake your own autopoem at jarodhargreav.es/autopoetry"
-//         let whatsAppText = `${this.poemTitle}\n\n`
-
-//         this.poemContents.each(function() {
-//             whatsAppText += `${this.textContent}\n`
-//         })
-
-//         const whatsAppLink =
-//             whatsAppPre + encodeURI(whatsAppText + whatsAppPost)
-
-//         return whatsAppLink
-//     }
-
-//     sendWhatsAppPoem() {
-//         const that = this
-//         this.shareLink.parent().attr("href", that.whatsAppPoemLink())
-//     }
-// }
