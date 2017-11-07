7b2a3dfa2e769cae640e42fddf236ff39ece4003
Jarod <jarod.hargreaves@gmail.com>
2017-11-07 23:19:22 +0000

    Rearrange icons for css hover effects

--- app/assets/scripts/components/poem_line.js
+++ app/assets/scripts/components/poem_line.js
@@ -3,10 +3,10 @@ import {} from "../actions"
 
 const PoemLine = ({content, index, onClick}) => (
     <li tabIndex="0" className="poem-line">
-        <span className="poem-line--text">{content}</span>
-        <div className="poem-line__refresh">
-            <i className="material-icons md-18" onClick={onClick}>cached</i>
+        <div className="poem-line__refresh" onClick={onClick}>
+            <i className="material-icons md-18">cached</i>
         </div>
+        <span className="poem-line__text">{content}</span>
     </li>
 )
 
